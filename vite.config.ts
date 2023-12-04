cc5f84d37a432bfb63f8e08bc03f72b4ca843cae
Phil Weir <phil.weir@flaxandteal.co.uk>
2023-12-04 20:01:16 +0000

    provide searchable forms

--- vite.config.ts
+++ vite.config.ts
@@ -11,7 +11,7 @@ export default defineConfig({
     VitePWA({ registerType: 'autoUpdate' }),
     legacy()
   ],
-  base: "./",
+  base: "/flashpwa/",
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
