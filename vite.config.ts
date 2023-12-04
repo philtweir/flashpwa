ea44c9614468231643ccdddd82c988d621727c29
Phil Weir <phil.weir@flaxandteal.co.uk>
2023-12-04 17:27:19 +0000

    correct base url

--- vite.config.ts
+++ vite.config.ts
@@ -11,6 +11,7 @@ export default defineConfig({
     VitePWA({ registerType: 'autoUpdate' }),
     legacy()
   ],
+  base: "./",
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
